92bca34a9d6d925cd157001e822ea210772516fe
Poovarasan R <poovarasan.mern@gmail.com>
2025-10-31 01:59:05 +0530

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectDB();
 // Middlewares
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://mymovie-frontend.onrender.com"],
   credentials: true,
 }));
 
